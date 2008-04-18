02f12b82ef25544eca4aaa5c7a240e06321bae23
Marco Secchi <marco.secchi@puremvc.org>
2008-04-18 17:34:03 +0000

    first release

---
+++ src/org/puremvc/haxe/examples/simpleAdmin/ui/IMenu.hx
@@ -0,0 +1,10 @@
+/* 
+ PureMVC haXe Demo - Multiplatform Communications
+ PureMVC - Copyright(c) 2008Marco Secchi <marco.secchi@puremvc.org> 
+ Your reuse is governed by the Creative Commons Attribution 3.0 License 
+ */
+package org.puremvc.haxe.examples.simpleAdmin.ui;
+
+interface IMenu
+{
+}
